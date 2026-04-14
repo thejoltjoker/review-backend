977dff65daed3e066fb0fed686346efdf6129451
Johannes <hello@thejoltjoker.com>
2026-04-14 16:54:28 +0200

    feat: finish CRUD on `ProjectsController`

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -3,21 +3,59 @@ using Review.Api.Models;
 using Review.Api.Services;
 
 namespace Review.Api.Controllers;
+
 // TODO add authorization
 [ApiController]
 [Route("[controller]")]
 public class ProjectsController(IProjectService service) : ControllerBase
 {
     private readonly IProjectService _service = service;
-    
+
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Project>>> GetAll()
     {
         // TODO implement global exception handler
         try
         {
-            IEnumerable<Project> projects = await _service.GetAllAsync();
-            return Ok(projects);
+            IEnumerable<Project> result = await _service.GetAllAsync();
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<Project>> GetById(string id)
+    {
+        // TODO implement global exception handler
+        try
+        {
+            Project? result = await _service.GetByIdAsync(id);
+            if (result == null) return NotFound($"Project {id} not found");
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Project>> Create(Project project)
+    {
+        // TODO implement global exception handler
+        // TODO validate data
+        try
+        {
+            var result = await _service.CreateAsync(project);
+            // if (result == null) return BadRequest("Project couldn't be created");
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, project);
         }
         catch (Exception e)
         {
@@ -26,16 +64,35 @@ public class ProjectsController(IProjectService service) : ControllerBase
         }
     }
     
-    [HttpGet]
+    [HttpPut]
     [Route("{id}")]
-    public async Task<ActionResult<Project>> GetById(string id)
+    public async Task<ActionResult> Update(string id, Project project)
+    {
+        // TODO implement global exception handler
+        // TODO validate data
+        try
+        {
+            var result = await _service.UpdateAsync(id, project);
+            if (!result) return BadRequest($"Couldn't update project {id}");
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<ActionResult> Delete(string id)
     {
         // TODO implement global exception handler
         try
         {
-            Project? project = await _service.GetByIdAsync(id);
-            if (project == null) return NotFound($"Project {id} not found");
-            return Ok(project);
+            var result = await _service.DeleteAsync(id);
+            if (!result) return BadRequest($"Couldn't delete project {id}");
+            return NoContent();
         }
         catch (Exception e)
         {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Review.Api;
 using Review.Api.Contexts;
 using Review.Api.Models;
 using Review.Api.Repositories;
